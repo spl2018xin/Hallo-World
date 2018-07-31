69ddddebec4b906bf1ce4f122ba33368dbd43ca1
spl2018xin <39158621+spl2018xin@users.noreply.github.com>
2018-07-31 15:55:38 +0200

    Update replicate the Fama French 3 factor model.R

--- Code/replicate the Fama French 3 factor model.R
+++ Code/replicate the Fama French 3 factor model.R
@@ -4,95 +4,94 @@
 #                                                                #
 ##################################################################
 
-#1# Date Preparation
+#1# Data preparation
 
-library(readxl)
+library (readxl)
 
 # FF3 is the monthly data collection of the three factors from 1963/07 to 1991/12
-FF3 <- read_excel("Data/FF3_196307-199112.xlsx")
+FF3 = read_excel ("Data/FF3_196307-199112.xlsx")
 
 # P25 is value-weighted 5*5 portfolios from 1963/07 to 1991/12
-P25 <- read_excel("Data/FF3_25_ValueWeighted.xlsx")
+P25 = read_excel ("Data/FF3_25_ValueWeighted.xlsx")
 
 
-#2# read regression factors
+#2# Read regression factors
 
-# unlist: convert the data into vector format
-rmrf <- unlist (FF3[, 2])
-smb <- unlist (FF3[, 3])
-hml <- unlist (FF3[, 4])
-rf <- unlist (FF3[, 5])
-ri <- unlist (P25[, 2])
-rirf <- unlist (ri-rf) # excess return of the target fund
+# Unlist: convert the data into vector format
+rmrf = unlist (FF3[, 2])
+smb = unlist (FF3[, 3])
+hml = unlist (FF3[, 4])
+rf = unlist (FF3[, 5])
+ri = unlist (P25[, 2])
+rirf = unlist (ri-rf) # excess return of the target fund
 rirf 
 
 
 #3# Start batch regressing 25 portfolios 
 
-results <- list()
+results = list()
 
 # Data starts from the 2nd col of P25
-for (i in 1:(ncol(P25) - 1)) { 
-  rirf <- unlist (P25[, i + 1]) - rf   
-     y <- lm (rirf ~ rmrf + smb + hml)
-  results[[i]] <- summary (y)
+for (i in 1: (ncol(P25) - 1)) { 
+  rirf = unlist (P25[, i + 1]) - rf   
+     y = lm (rirf ~ rmrf + smb + hml)
+  results[[i]] = summary (y)
 }
 
+# Set up empty vectors
 
-# set up empty vectors
+betas = vector ()
+std.errors = vector ()
+t.values = vector ()
+R.squareds = vector ()
 
-betas <- vector ()
-std.errors <- vector ()
-t.values <- vector ()
-R.squareds <- vector ()
-
-# save all betas' regression results
+# Save all betas' regression results
 
 for(i in 1: (ncol (P25) - 1)) {
-       betas <- cbind (betas, results[[i]]$coefficients[, 1])
-  std.errors <- cbind (std.errors,results[[i]]$sigma)
-    t.values <- cbind (t.values, results[[i]]$coefficients[, 3])
-  R.squareds <- cbind (R.squareds, results[[i]]$adj.r.squared)
+       betas = cbind (betas, results[[i]]$coefficients[, 1])
+  std.errors = cbind (std.errors,results[[i]]$sigma)
+    t.values = cbind (t.values, results[[i]]$coefficients[, 3])
+  R.squareds = cbind (R.squareds, results[[i]]$adj.r.squared)
 }
 
-# give names
-colnames(betas) <- colnames(P25)[-1]
+# Give names
+colnames (betas) = colnames (P25)[-1]
 
 
-#4#   Resize the output to 5x5 format like Fama French paper
-resize <- function(x) {
+#4# Resize the output to 5x5 format like Fama French paper
+resize = function (x) {
   return (matrix (x, nrow=5, byrow = TRUE, 
-                  dimnames = list (c("SMALL", "2", "3", "4", "BIG"), c("LOW", "2", "3", "4", "HIGH"))))
+                  dimnames = list (c ("SMALL", "2", "3", "4", "BIG"), c ("LOW", "2", "3", "4", "HIGH"))))
 }
 
-# resize alpha (intercept)
-alpha <- resize(betas[1, ])
+# Resize alpha (intercept)
+alpha = resize (betas[1, ])
 alpha
 
-# resize beta
-market.beta <- resize(betas[2, ])
-SMB.beta <- resize(betas[3, ])
-HML.beta <- resize(betas[4, ])
+# Resize beta
+market.beta = resize (betas[2, ])
+SMB.beta = resize (betas[3, ])
+HML.beta = resize (betas[4, ])
 
-# display betas
+# Display betas
 market.beta
 SMB.beta
 HML.beta
 
-# resize t-stats
-market.t <-resize(t.values[2,])
-SMB.t <- resize(t.values[3,])
-HML.t <- resize(t.values[4,])
+# Resize t-stats
+market.t = resize (t.values[2,])
+SMB.t = resize (t.values[3,])
+HML.t = resize (t.values[4,])
 market.t
 
-# resize R-squareds
-resize(R.squareds)
-resize(std.errors)
+# Resize R-squareds
+resize (R.squareds)
+resize (std.errors)
 
-# create a table with all coefficients and t statistics
-coef.t <- cbind (rbind (alpha, market.beta, SMB.beta, HML.beta), 
-                 rbind(resize(t.values[1,]), market.t, SMB.t, HML.t))
-View(coef.t)
+# Create a table with all coefficients and t statistics
+coef.t = cbind (rbind (alpha, market.beta, SMB.beta, HML.beta), 
+                 rbind (resize (t.values[1,]), market.t, SMB.t, HML.t))
+View (coef.t)
 
 
 
